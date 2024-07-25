5c4efe2d3049234c758b2facd46391f8ca4d75d9
narr07 <dinarpermadi07@gmail.com>
2024-07-25 16:08:25 +0700

    chore: Add ESLint configuration and update Nuxt dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    'nuxt-delay-hydration',
+    '@nuxt/fonts',
+    '@nuxtjs/seo',
+    '@nuxthq/studio',
+    '@vueuse/nuxt',
+  ],
 })
