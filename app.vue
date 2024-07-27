012d57458a388a7b1fd6515da5ad91292c6153dc
narr07 <dinarpermadi07@gmail.com>
2024-07-27 10:17:09 +0700

    chore: Update Nuxt configuration and add Tailwind CSS associations

--- app.vue
+++ app.vue
@@ -4,5 +4,27 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <!-- contoh nuxt img -->
+
+  <!-- <div>
+
+    <NuxtImg
+      src="/path/to/image.jpg"
+      preset="article"
+      sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+    />
+
+    <NuxtImg
+      src="/path/to/image.jpg"
+      preset="galeriKegiatan"
+      sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+    />
+
+    <NuxtImg
+      src="/path/to/image.jpg"
+      preset="profilGuru"
+      sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 33vw"
+    />
+  </div> -->
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,94 @@ export default defineNuxtConfig({
     '@nuxthq/studio',
     '@vueuse/nuxt',
   ],
+  site: {
+    url: 'https://sdnteja2.sch.id',
+    name: 'SDN Teja 2',
+    description: 'Selamat datang di situs resmi SDN Teja 2! Dapatkan informasi terbaru dan penting mengenai sekolah kami.',
+    defaultLocale: 'id', // Bahasa default Indonesia
+  },
+  content: {
+    documentDriven: true,
+  },
+  ui: {
+    global: true,
+  },
+  colorMode: {
+    preference: 'dark',
+  },
+  fonts: {
+    families: [
+      {
+        name: 'Poppins',
+        provider: 'google',
+        weights: [400, 600, 700, 800, 900], // Anda bisa menambahkan bobot yang diperlukan
+        styles: ['normal', 'italic'],
+      },
+      {
+        name: 'Lato',
+        provider: 'google',
+        weights: [300, 400, 700, 900], // Anda bisa menambahkan bobot yang diperlukan
+        styles: ['normal', 'italic'],
+      },
+    ],
+  },
+  image: {
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+    },
+    presets: {
+      avatar: {
+        modifiers: {
+          format: 'webp',
+          width: 64,
+          height: 64,
+        },
+      },
+      thumbnail: {
+        modifiers: {
+          format: 'webp',
+          width: 150,
+          height: 150,
+          quality: 80,
+        },
+      },
+      gallery: {
+        modifiers: {
+          format: 'webp',
+          width: 1024,
+          height: 768,
+          quality: 85,
+        },
+      },
+      profile: {
+        modifiers: {
+          format: 'webp',
+          width: 400,
+          height: 400,
+          quality: 90,
+        },
+      },
+      card: {
+        modifiers: {
+          format: 'webp',
+          width: 600,
+          height: 900,
+          quality: 75,
+        },
+      },
+      wide: {
+        modifiers: {
+          format: 'webp',
+          width: 1200,
+          height: 800,
+          quality: 80,
+        },
+      },
+    },
+  },
 })
