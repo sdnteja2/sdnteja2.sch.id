cb655125dea673f27df6a5f5e053e1c74d9d98c4
narr07 <dinarpermadi07@gmail.com>
2025-12-22 20:43:01 +0700

    feat: integrate @nuxthub/core-nightly and configure D1 database
    - Added @nuxthub/core-nightly dependency in package.json.
    - Updated nuxt.config.ts to include D1 database configuration.
    - Removed wrangler.toml and replaced it with wrangler.jsonc for better structure and clarity.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/ui',
     'nuxt-studio',
+    '@nuxthub/core-nightly',
   ],
    css: ['~/assets/css/main.css'],
    ui: {
@@ -18,6 +19,14 @@ export default defineNuxtConfig({
       type: 'd1',
       bindingName: 'teja2'
     }
+  },
+    hub: {
+    // D1 database
+    db: {
+      dialect: 'sqlite',
+      driver: 'd1',
+      connection: { databaseId: '21086ffa-4f15-4181-8cb6-52dcb7f5c549' }
+    },
   },
   nitro: {
     preset: 'cloudflare_module',
